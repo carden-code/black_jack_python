0267bafd7ec0a4e1f113993be2da1b39db7e0ffa
Carden <carden.ruby@gmail.com>
2021-02-21 21:22:29 +0300

    not completed method take_card in Dealer

--- dealer.py
+++ dealer.py
@@ -1,6 +1,11 @@
 from user import User
+from deck import Deck
 
 
 class Dealer(User):
     def __init__(self, name='Dealer'):
         super().__init__(name)
+
+    # def take_card(self):
+    #     deck = Deck.deal_cards()
+    #     self.cards.append(deck)
--- deck.py
+++ deck.py
@@ -8,9 +8,15 @@ class Deck:
         self.creates_deck_cards()
         shuffle(self.cards)
 
+    # Наполняет колоду объектами карт.
     def creates_deck_cards(self):
         suits = Card.SUITS
         values = Card.VALUES
         for suit in suits:
             for value in values:
                 self.cards.append(Card(suit, value))
+
+    # Выдаёт карту из калоды.
+    def deal_cards(self):
+        card = self.cards.pop()
+        return card
