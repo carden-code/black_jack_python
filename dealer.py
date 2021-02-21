8223b20b737d8250cf69f3e75b9f5c94b4bb9dc7
Carden <carden.ruby@gmail.com>
2021-02-22 01:32:58 +0300

    completed method take_card

--- dealer.py
+++ dealer.py
@@ -1,11 +1,10 @@
 from user import User
-from deck import Deck
 
 
 class Dealer(User):
     def __init__(self, name='Dealer'):
         super().__init__(name)
 
-    # def take_card(self):
-    #     deck = Deck.deal_cards()
-    #     self.cards.append(deck)
+    def take_card(self, deck):
+        card = deck.deal_cards()
+        self.cards.append(card)
