7b19e04295a635f9351f0ffbe4d6fa43955ea458
Carden <carden.ruby@gmail.com>
2021-02-23 20:06:08 +0300

    add method adjusting card points

--- game.py
+++ game.py
@@ -1,1 +1,25 @@
+from dealer import Dealer
+from user import User
+from deck import Deck
+from bank import Bank
 
+
+class Game:
+    def __init__(self, user):
+        self.user = user
+        self.dealer = Dealer()
+        self.deck = Deck()
+        self.bank = Bank()
+
+    def new_round(self):
+        self.deck = Deck()
+        self.user.cards = []
+        self.dealer.cards = []
+        self.user.take_card(self.deck)
+        self.user.take_card(self.deck)
+        self.dealer.take_card(self.deck)
+        self.dealer.take_card(self.deck)
+        self.user.calculate_amount_points()
+        self.dealer.calculate_amount_points()
+
+    def determine_winner(self):
--- user.py
+++ user.py
@@ -2,7 +2,7 @@ from bank import Bank
 
 
 class User:
-    def __init__(self, name):
+    def __init__(self, name='User'):
         self.name = name
         self.cards = []
         self.money = 100
@@ -19,7 +19,15 @@ class User:
     def take_card(self, deck):
         card = deck.deal_cards()
         self.cards.append(card)
+        self.calculate_amount_points()
 
     def calculate_amount_points(self):
-        for card in self.cards:
-            self.sum_cards += card.values
+        self.sum_cards = sum([card.values for card in self.cards])
+        self.adjusting_card_points()
+
+    def adjusting_card_points(self):
+        if self.sum_cards > 21:
+            for card in self.cards[::-1]:
+                if card.is_ace():
+                    self.sum_cards -= 10
+
